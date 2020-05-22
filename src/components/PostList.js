526b29b7e81c3e9e90b3075c2891b580e7d94a70
Mitch Lum <mklum88@gmail.com>
2020-05-21 20:27:49 -0700

    added async function with redux-thunk

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,7 +4,7 @@ import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
   componentDidMount() {
-    return this.props.fetchPosts;
+    this.props.fetchPosts();
   }
 
   render() {

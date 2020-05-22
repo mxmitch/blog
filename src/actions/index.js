044f409853df1ad141a379af968b9ca01f5cf2d1
Mitch Lum <mklum88@gmail.com>
2020-05-22 16:27:17 -0700

    refactor UserHeader to remove fetchUser call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async (dispatch) => {
